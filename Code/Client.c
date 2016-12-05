3f4a4aedc19c3140405e0a4def58bef598cc6b3f
TheByrne <byrne.jaymes@gmail.com>
2016-12-05 12:16:02 -0800

    - send and receive separate

--- Code/Client.c
+++ Code/Client.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <pthread.h>
 
 #define SERVER "127.0.0.1"
 #define BUFLEN 512  //Max length of buffer
@@ -20,8 +21,13 @@ char buf[BUFLEN];
 char bufLis[BUFLEN];
 char message[BUFLEN];
 
+pthread_mutex_t lock;
+
 
 void *talking_function(void *arg); //  function for talking thread
+
+void *listen_function(void *arg); //  function for talking thread
+
 
 void die(char *s)
 {
@@ -32,8 +38,8 @@ void die(char *s)
 int main(void)
 {
     
-     int status;
-     pthread_t	tid[1]; // init my one threads
+    int status;
+    pthread_t	tid[2]; // init my one threads
     
     if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
@@ -56,13 +62,21 @@ int main(void)
         perror("Thread create error");
         exit(EXIT_FAILURE);
     }
-    status = pthread_join(tid[0], NULL);
+    status = pthread_create(&(tid[1]), NULL, listen_function, NULL);
+    
+    if (status != 0)
+    {
+        perror("Thread create error");
+        exit(EXIT_FAILURE);
+    }
+
+    status = pthread_join(tid[1], NULL);
     if (status != 0)
     {
         perror("Thread join error");
         exit(EXIT_FAILURE);
     }
-
+    
     
     close(s);
     return 0;
@@ -71,9 +85,11 @@ void *talking_function(void *arg)
 {
     while(1)
     {
-        printf("Enter message : ");
+        pthread_mutex_lock(&lock); // has the lock
+        printf("\n Enter message : \n");
         gets(message);
         
+
         //send the message
         if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
         {
@@ -84,14 +100,36 @@ void *talking_function(void *arg)
         //clear the buffer by filling null, it might have previously received data
         memset(buf,'\0', BUFLEN);
         //try to receive some data, this is a blocking call
-        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
+       
+        printf("\n");
+        pthread_mutex_unlock(&lock);//released
+    }
+    
+    
+    pthread_exit(NULL);
+}
+void *listen_function(void *arg)
+{
+    while(1)
+    {
+        //receive a reply and print it
+        //clear the buffer by filling null, it might have previously received data
+       pthread_mutex_lock(&lock); // has the lock
+        memset(bufLis,'\0', BUFLEN);
+        //try to receive some data, this is a blocking call
+        if (recvfrom(s, bufLis, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
         {
             die("recvfrom()");
         }
         
-        puts(buf);
+        
+        
+         puts(bufLis);
+          printf("  --- recieved \n");
+          pthread_mutex_unlock(&lock);//released
+        
     }
-
+    
     
     pthread_exit(NULL);
 }
